531e9f3c4c1d5c0448e3950ae3d7a0fef86ccd4f
Rachel Wyatt <rw126@evansville.edu>
2018-11-21 16:31:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -81,7 +81,7 @@ class Dog
     def self.find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
       if !dog.empty?
-            self.update
+          update
       else
         dog = self.create(name: name, breed: breed)
       end
